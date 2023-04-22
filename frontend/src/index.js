36adf76a50111458c3d8b5896859ed81028990b7
BrendonCurmi <brendoncrm@gmail.com>
2023-04-23 00:11:24 +0200

    Implement AuthProvider in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+import { AuthProvider } from "./context/AuthProvider";
+
 import "./index.module.scss";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <AuthProvider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </AuthProvider>,
     document.getElementById("app"));
