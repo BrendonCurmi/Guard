68453eff69fd056167c44a025caadfb37f498a48
BrendonCurmi <brendoncrm@gmail.com>
2023-04-22 21:03:59 +0200

    Create App component to render React root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render("Test", document.getElementById("app"));
+import App from "./App";
+ReactDOM.render(<App/>, document.getElementById("app"));
