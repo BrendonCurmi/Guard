5327437c2901525b07e64b6b1b72b0e7e44faeba
BrendonCurmi <brendoncrm@gmail.com>
2023-04-22 23:57:18 +0200

    Add NotFound route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </div>
     );
