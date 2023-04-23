ede9e57d91e6c444de985de85750acb57a2a1dbd
BrendonCurmi <brendoncrm@gmail.com>
2023-04-23 15:50:03 +0200

    Add FA icons to navigation

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faRightToBracket, faRightFromBracket, faUser } from "@fortawesome/free-solid-svg-icons";
+
 /**
  * Creates the Navigation of the app.
  * @param props nav properties.
@@ -9,18 +12,22 @@ import { Link } from "react-router-dom";
 const Navigation = (props) => {
     const auth = props.auth;
     const loginStatus = auth ? <Link to="/logout">Logout</Link> : <Link to="/login">Login</Link>;
+    const loginIcon = auth ? faRightFromBracket: faRightToBracket;
     return (
         <header>
             <div>
                 <ul>
                     <li>
+                        <FontAwesomeIcon icon={faHouse}/>
                         <Link to="/">Home</Link>
                     </li>
                     <li>
+                        <FontAwesomeIcon icon={loginIcon}/>
                         {loginStatus}
                     </li>
                     {auth && (
                         <li>
+                            <FontAwesomeIcon icon={faUser}/>
                             <a>Logged in</a>
                         </li>
                     )}
