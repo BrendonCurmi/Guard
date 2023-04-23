2aa9f8b79da28c98f794ae6b9ff06af8fc755c38
BrendonCurmi <brendoncrm@gmail.com>
2023-04-23 13:05:34 +0200

    Create RestrictedRoute component to restrict access to components

---
+++ frontend/src/components/RestrictedRoute.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+/**
+ * Creates a route restricted to the passed "auth" property.
+ * If access is authorised, the Route child component is rendered.
+ * If access is not authorised, redirect to /login route.
+ * @constructor
+ */
+const RestrictedRoute = (props) => {
+    return props.auth ? <Outlet/> : <Navigate to="/login"/>;
+}
+
+export default RestrictedRoute;
