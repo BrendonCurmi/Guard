f0b4aa7799cc805b9f9e6d0182a62be34cad0605
BrendonCurmi <brendoncrm@gmail.com>
2023-04-23 16:57:03 +0200

    Add Navigation to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import Navigation from "./components/navigation/Navigation";
+
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
+import { useAuth } from "./context/AuthProvider";
+
 const App = () => {
+    const { auth } = useAuth();
     return (
         <div>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
+            <Navigation auth={auth} email/>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
 
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </main>
         </div>
     );
 };
