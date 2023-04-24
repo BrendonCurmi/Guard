b495f5e3353c4143302d4a878e70d50a975a028d
BrendonCurmi <brendoncrm@gmail.com>
2023-04-24 13:39:18 +0200

    Add NotFound css

--- frontend/src/pages/NotFound/index.js
+++ frontend/src/pages/NotFound/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import classes from "./index.module.scss";
+
 const NotFound = () => {
     return (
-        <div>
+        <div className={classes.notFound}>
             <h1>404 Page Not Found</h1>
             <Link to="/">Homepage</Link>
         </div>
