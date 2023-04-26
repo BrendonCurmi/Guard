c40a98239d97e484a67f4ccf98a85e11a4727a6a
BrendonCurmi <brendoncrm@gmail.com>
2023-04-26 14:23:47 +0200

    Add hover state to ListedViewItem for action buttons

--- frontend/src/components/views/ListedViewItem.js
+++ frontend/src/components/views/ListedViewItem.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCopy, faEye, faEyeSlash, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 import useIcon from "../../hooks/use-icon";
 
 import { describeDate } from "../../utils/DateUtils";
 
-import classes from "../../pages/Passwords/index.module.scss";
+import classes from "./ListedViewItem.module.scss";
 
-const ListedViewItem = ({ dKey, account, icon, onEditClickHandler, onCopyClick, onShowClick, setConfirming }) => {
+const ListedViewItem = ({ dKey, account, icon, onEditClickHandler, onCopyClick, setConfirming }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     const key = dKey;// "key" cannot be a prop
     const focusedIcon = useIcon(icon, account.site);
     return (
-        <tr key={account.title}>
+        <tr key={account.title}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
             <td onClick={() => onEditClickHandler(key)}>
                 {focusedIcon}
             </td>
@@ -22,18 +26,17 @@ const ListedViewItem = ({ dKey, account, icon, onEditClickHandler, onCopyClick, 
             </td>
             <td onClick={() => onEditClickHandler(key)}>{describeDate(account.lastAccess)}</td>
             <td>
-                <button onClick={() => onCopyClick(key)}>
-                    <FontAwesomeIcon icon={faCopy}/>
-                </button>
-                {/*<button onClick={() => onShowClick(key)}>
-                    <FontAwesomeIcon icon={account.editing ? faEyeSlash : faEye}/>
-                </button>*/}
-                <button onClick={() => onEditClickHandler(key)}>
-                    <FontAwesomeIcon icon={faPencil}/>
-                </button>
-                <button onClick={() => setConfirming(key)}>
-                    <FontAwesomeIcon icon={faTrash}/>
-                </button>
+                {isHovered && <>
+                    <button onClick={() => onCopyClick(key)}>
+                        <FontAwesomeIcon icon={faCopy}/>
+                    </button>
+                    <button onClick={() => onEditClickHandler(key)}>
+                        <FontAwesomeIcon icon={faPencil}/>
+                    </button>
+                    <button onClick={() => setConfirming(key)}>
+                        <FontAwesomeIcon icon={faTrash}/>
+                    </button>
+                </>}
             </td>
         </tr>
     );
