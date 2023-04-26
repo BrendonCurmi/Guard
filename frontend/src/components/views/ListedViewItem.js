6f99ba873fa4a6a353e4da16c16c343eed6cc3d1
BrendonCurmi <brendoncrm@gmail.com>
2023-04-26 17:26:39 +0200

    Add customisation props to ListedViewItem

--- frontend/src/components/views/ListedViewItem.js
+++ frontend/src/components/views/ListedViewItem.js
@@ -8,36 +8,69 @@ import { describeDate } from "../../utils/DateUtils";
 
 import classes from "./ListedViewItem.module.scss";
 
-const ListedViewItem = ({ dKey, account, icon, onEditClickHandler, onCopyClick, setConfirming }) => {
+const ListedViewItem = ({ dKey, account, icon, onEditClickHandler, onCopyClick, setConfirming, listedViewProps }) => {
     const [isHovered, setIsHovered] = useState(false);
 
+    listedViewProps = listedViewProps || {
+        cells: {
+            time: "lastAccess"
+        },
+        canCopy: true,
+        canEdit: true,
+        canDelete: true,
+        customActionBtns: ""
+    };
+
+    const { cells, canCopy, canEdit, canDelete, customActionBtns } = listedViewProps;
+    const { time } = cells;
+
     const key = dKey;// "key" cannot be a prop
-    const focusedIcon = useIcon(icon, account.site);
+    const { site, title } = account;
+    const focusedIcon = useIcon(icon, site);
+
+    const onClick = () => {
+        return canEdit ? () => onEditClickHandler(key) : "";
+    };
+
+    const DefaultActionBtns = () => {
+        const copyBtn =
+            <button onClick={() => onCopyClick(key)}>
+                <FontAwesomeIcon icon={faCopy}/>
+            </button>;
+
+        const editBtn =
+            <button onClick={onClick}>
+                <FontAwesomeIcon icon={faPencil}/>
+            </button>;
+
+        const deleteBtn =
+            <button onClick={() => setConfirming(key)}>
+                <FontAwesomeIcon icon={faTrash}/>
+            </button>;
+        return (
+            <>
+                {customActionBtns}
+                {canCopy && copyBtn}
+                {canEdit && editBtn}
+                {canDelete && deleteBtn}
+            </>
+        );
+    };
+
+    const actionBtns = <DefaultActionBtns/>;
     return (
-        <tr key={account.title}
+        <tr key={title}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}>
-            <td onClick={() => onEditClickHandler(key)}>
+            <td onClick={onClick}>
                 {focusedIcon}
             </td>
-            <td onClick={() => onEditClickHandler(key)}>
+            <td onClick={onClick}>
                 <p className={classes.accountTitle}>{account.title}</p>
                 <p className={classes.accountSubtitle}>{account.identity ? account.identity : ""}</p>
             </td>
-            <td onClick={() => onEditClickHandler(key)}>{describeDate(account.lastAccess)}</td>
-            <td>
-                {isHovered && <>
-                    <button onClick={() => onCopyClick(key)}>
-                        <FontAwesomeIcon icon={faCopy}/>
-                    </button>
-                    <button onClick={() => onEditClickHandler(key)}>
-                        <FontAwesomeIcon icon={faPencil}/>
-                    </button>
-                    <button onClick={() => setConfirming(key)}>
-                        <FontAwesomeIcon icon={faTrash}/>
-                    </button>
-                </>}
-            </td>
+            <td onClick={onClick}>{describeDate(account[time])}</td>
+            <td>{isHovered && actionBtns}</td>
         </tr>
     );
 };
