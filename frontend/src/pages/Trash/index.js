0805617ae4ba4d5ea2fecdb1325217ba6e7e2511
BrendonCurmi <brendoncrm@gmail.com>
2023-04-26 17:33:49 +0200

    Migrate Trash page to implement FullView with listedViewProps

--- frontend/src/pages/Trash/index.js
+++ frontend/src/pages/Trash/index.js
@@ -1,94 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrashRestore } from "@fortawesome/free-solid-svg-icons";
 
-import SiteIcon from "../../components/SiteIcon";
-import ConfirmView from "../../components/views/popups/ConfirmView";
-import ListedView from "../../components/views/ListedView";
-
-import { describeDate } from "../../utils/DateUtils";
+import FullView from "../../components/views/FullView";
 
 import classes from "./index.module.scss";
 
 const API = "http://localhost:4000/api/trash";
 
 const Trash = () => {
-    const [accounts, setAccounts] = useState([]);
-    const [confirming, setConfirming] = useState(null);
-
-    // Load accounts after loading component
-    useEffect(() => {
-        getAccounts();
-    }, []);
-
-    /**
-     * Get accounts.
-     */
-    const getAccounts = async () => {
-        const res = await fetch(API);
-        const accountData = await res.json();
-        console.log("API REQUEST SHOULD BE DONE ONCE")
-        setAccounts(accountData);
-    };
-
-
-    const deleteAccountOnConfirmationHandler = async () => {
-        if (confirming === null) return;
-        const rawResponse = await fetch(`http://localhost:4000/api/account/${accounts[confirming]._id}`, {
-            method: "DELETE"
-        });
-
-        if (rawResponse.status === 200) {
-            const content = await rawResponse.json();
-            // console.log(content);
-            await getAccounts();
-            setConfirming(null);
-        }
-    };
-
-    const Popup = () => {
+    const ActionBtns = () => {
+        const deleteBtn =
+            <button onClick={() => console.log("todo")}>
+                <FontAwesomeIcon icon={faTrashRestore}/>
+            </button>;
         return (
-            confirming !== null &&
-            <ConfirmView className={classes.viewWrapper}
-                         onCancel={() => setConfirming(null)}
-                         onConfirm={deleteAccountOnConfirmationHandler}>
-                <h2>Are you sure?</h2>
-                <h4>Do you really want to delete this account? It can't be recovered once it's deleted</h4>
-            </ConfirmView>
+            <>
+                {deleteBtn}
+            </>
         );
     };
 
-    return (
-        <ListedView
-            shade={confirming !== null}
-            pageTitle="Trash"
-            pageAction="Clear Trash"
-            timeName="Date Deleted"
-            popups={<Popup/>}>
-
-            {accounts && accounts.map((account, key) => (
-                <tr key={key}>
-                    <td>
-                        <SiteIcon domain={account.site} size="32"/>
-                    </td>
-                    <td>
-                        <p>{account.title}</p>
-                        {/*<p>{product.email || product.username}</p>*/}
-                        <p>test</p>
-                    </td>
-                    <td>{describeDate(account.deletedDate)}</td>
-                    <td>
-                        <button>
-                            <FontAwesomeIcon icon={faPencil}/>
-                        </button>
-                        <button onClick={() => setConfirming(key)}>
-                            <FontAwesomeIcon icon={faTrash}/>
-                        </button>
-                    </td>
-                </tr>
-            ))}
-        </ListedView>
-    );
+    return <FullView
+        page={{ title: "Trash", action: "Clear Trash", timeName: "Date Deleted" }}
+        confirm={{
+            title: "Are you sure you want to permanently delete this?",
+            msg: "This cannot be recovered once deleted"
+        }}
+        loadApi={API}
+        deleteItemHandler={() => console.log("todo")}
+        listedViewProps={{
+            cells: { time: "deletedDate" },
+            canCopy: false,
+            canEdit: false,
+            canDelete: true,
+            customActionBtns: <ActionBtns/>
+        }}
+    />;
 };
 
 export default Trash;
