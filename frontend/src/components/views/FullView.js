14f9a6fa12095b77c9d5d9dff981960d28a9b8ba
BrendonCurmi <brendoncrm@gmail.com>
2023-04-26 16:48:28 +0200

    Rewrite FullView

--- frontend/src/components/views/FullView.js
+++ frontend/src/components/views/FullView.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
 
 import ListedView from "./ListedView";
 import ListedViewItem from "./ListedViewItem";
-import FocusedView from "../passwords/FocusedView";
-import Confirm from "../confirm/Confirm";
+import Popups from "./Popups";
 import { getData } from "../passwords/Profile";
 
 import { copyToClipboard } from "../../utils/CopyUtils";
@@ -13,59 +13,25 @@ import { copyToClipboard } from "../../utils/CopyUtils";
  * @param page the page variables.
  * @param confirm the confirm variables.
  * @param icon the icon.
- * @param dataType the item type.
+ * @param loadApi the API to load items.
+ * @param dataType the optional item type.
+ * @param deleteItemHandler the optional delete handler.
+ * @param loadDeps the dependencies for reloading items.
  * @returns {JSX.Element}
  */
 const FullView = ({
                       page = { title: "", action: "", timeName: "" },
                       confirm = { title: "", msg: "" },
                       icon,
-                      dataType
+                      loadApi,
+                      dataType,
+                      deleteItemHandler,
+                      loadDeps = []
                   }) => {
     const [items, setItems] = useState([]);
-    const [isEditing, setIsEditing] = useState(false);
-    const [focused, setFocused] = useState();
-    const [confirming, setConfirming] = useState(null);
 
     /**
-     * Submit create/update requests, reload all items, and close FocusedView.
-     * @param data the data to create/update.
-     */
-    const submitItemRequest = async (data) => {
-        const rawResponse = await (focused ? callUpdateItem(focused._id, data) : callCreateItem(data));
-        if (rawResponse.status === 201 || rawResponse.status === 204) {
-            // const content = await rawResponse.json();
-            await loadAllItems();
-            switchFocusedViewHandler();
-        }
-    };
-
-    const callCreateItem = (data) => {
-        return send(createApi, "POST", data);
-    };
-
-    const callUpdateItem = (id, data) => {
-        return send(updateApi(id), "PUT", data);
-    };
-
-    const send = (url, method, data) => {
-        return fetch(url, {
-            method: method,
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
-    };
-
-    // Load items after loading component
-    useEffect(() => {
-        loadAllItems();
-    }, []);
-
-    /**
-     * Load all items.
+     * Load all items in the view.
      */
     const loadAllItems = async () => {
         const res = await fetch(loadApi);
@@ -74,18 +40,88 @@ const FullView = ({
         setItems(accountData);
     };
 
+    // Load items after loading component
+    useEffect(loadAllItems, [...loadDeps]);
+
+    // Actions
+    const onEditClickHandler = (key) => {
+        const [type, index] = key.split("-");
+        focusOn(items[type][index], type);
+    };
+
+    const onCopyClick = async (key) => {
+        const [type, index] = key.split("-");
+        const dataType = getData(type);
+        const creds = await getCreds(dataType, items[type][index]._id);
+        copyToClipboard(dataType.copyField(creds));
+    };
+
+    const getCreds = async (dataType, id) => {
+        const res = await fetch(dataType.endpoints.credentialsApi(id));
+        return await res.json();
+    };
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [focused, setFocused] = useState();
+    const [confirming, setConfirming] = useState(null);
+
+    const [dataTypeData, setData] = useState(false);
+
+    const focusOn = (item, type) => {
+        setFocused(item);
+        setIsEditing(prevState => !prevState);
+        if (type) setData(getData(type));
+    };
+
     /**
      * Clear focused item and either open or close view.
      */
     const switchFocusedViewHandler = () => {
-        setIsEditing(prevState => !prevState);
-        setFocused(null);
+        focusOn(null, dataType);
+    };
+
+    /**
+     * Submit create/update requests, reload all items, and close FocusedView.
+     * @param data the data to create/update.
+     */
+    const submitItemRequest = async (data) => {
+        const callCreateItem = (data) => {
+            // return send(createApi, "POST", data);
+            return send(dataTypeData.endpoints.createApi, "POST", data);
+        };
+
+        const callUpdateItem = (id, data) => {
+            // return send(updateApi(id), "PUT", data);
+            return send(dataTypeData.endpoints.updateApi(id), "PUT", data);
+        };
+
+        const send = (url, method, data) => {
+            return fetch(url, {
+                method: method,
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+        };
+
+
+        const rawResponse = await (focused ? callUpdateItem(focused._id, data) : callCreateItem(data));
+        if (rawResponse.status === 201 || rawResponse.status === 204) {
+            // const content = await rawResponse.json();
+            await loadAllItems();
+            switchFocusedViewHandler();
+        }
     };
 
 
     const deleteItemOnConfirmationHandler = async () => {
         if (confirming === null) return;
-        const rawResponse = await fetch(deleteApi(items[confirming]._id), { method: "DELETE" });
+        const [type, index] = confirming.split("-");
+        console.log("2", items[type][index]._id);
+        return;
+        const rawResponse = await fetch(dataTypeData.endpoints.deleteApi(items[type][index]._id), { method: "DELETE" });
         if (rawResponse.status === 200) {
             const content = await rawResponse.json();
             // console.log(content);
@@ -94,72 +130,62 @@ const FullView = ({
         }
     };
 
-    const onEditClickHandler = (key) => {
-        setFocused(items[key]);
-        setIsEditing(true);
-    };
-
-    const onCopyClick = async (key) => {
-        const creds = await getCreds(dataType, items[key]._id);
-        copyToClipboard(copyField(creds));
-    };
-
-    const onShowClick = (key) => {
-        setItems(prevState => {
-            prevState[key].editing = !prevState[key].editing;
-            return [...prevState];
+    const viewItems = Object.keys(items).map((type) => {
+        // Define type-specific props
+        const itemProps = {};
+        switch (type) {
+            case "accounts":
+                break;
+            case "pins":
+                itemProps.icon = faFolder;
+                break;
+        }
+        itemProps.type = type;
+        return items[type].map((item, itemIndex) => {
+            const key = `${type}-${itemIndex}`;
+            return <ListedViewItem
+                key={key} dKey={key}
+                account={item}
+                {...itemProps}
+                onEditClickHandler={onEditClickHandler}
+                onCopyClick={onCopyClick}
+                setConfirming={setConfirming}/>;
         });
+    });
+
+    const call = (defaultFn, optionalFn) => {
+        return optionalFn ? optionalFn : defaultFn;
     };
 
-    const getCreds = async (dataType, id) => {
-        const itemData = getData(dataType);
-        const res = await fetch(itemData.endpoints.credentialsApi(id));
-        return await res.json();
-    };
+    const popups = <Popups
+        className={classes.viewWrapper}
+        onCancelFocused={switchFocusedViewHandler}
+        onSubmitFocused={submitItemRequest}
+        onConfirmConfirm={call(deleteItemOnConfirmationHandler, deleteItemHandler)}
+        onCancelConfirm={() => setConfirming(null)}
 
-    const Popups = () => {
-        return (
-            <>
-                {isEditing &&
-                    <div>
-                        <FocusedView onClick={switchFocusedViewHandler}
-                                     focus={focused}
-                                     submitItemRequest={submitItemRequest}
-                                     getCreds={getCreds}
-                                     fields={fields}
-                                     icon={icon}
-                                     dataType={dataType}
-                        />
-                    </div>}
-                {confirming !== null &&
-                    <Confirm onCancel={() => setConfirming(null)}
-                             onConfirm={deleteItemOnConfirmationHandler}>
-                        <h2>{confirm.title}</h2>
-                        <h4>{confirm.msg}</h4>
-                    </Confirm>}
-            </>
-        );
-    };
+        isEditing={isEditing}
+        confirming={confirming}
+        focused={focused}
+
+        dataType={dataTypeData}
+        getCreds={getCreds}
+        focusedIcon={icon}
+        confirmVals={confirm}/>;
+
+    const isShade = isEditing || confirming !== null;
+    const pageActionClick = call(switchFocusedViewHandler, deleteItemHandler);
+    const { title, action, timeName } = page;
 
     return (
         <ListedView
-            shade={isEditing || confirming !== null}
-            pageTitle={page.title}
-            pageAction={page.action}
-            timeName={page.timeName}
-            pageActionClick={switchFocusedViewHandler}
-            popups={<Popups/>}>
-
-            {items && items.map((item, key) => (
-                <ListedViewItem
-                    key={key} dKey={key}
-                    account={item}
-                    icon={icon}
-                    onEditClickHandler={onEditClickHandler}
-                    onCopyClick={onCopyClick}
-                    onShowClick={onShowClick}
-                    setConfirming={setConfirming}/>
-            ))}
+            shade={isShade}
+            pageTitle={title}
+            pageAction={action}
+            timeName={timeName}
+            pageActionClick={pageActionClick}
+            popups={popups}>
+            {viewItems}
         </ListedView>
     );
 };
