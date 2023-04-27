6364856f3d1aeb7b3f22a7e827fa439669f586e8
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 19:48:34 +0200

    Add FolderView controller to folderRoutes

--- backend/folders/folderRoutes.js
+++ backend/folders/folderRoutes.js
@@ -12,4 +12,8 @@ router.route("/folder/:id")
     .put(controller.updateById)
     .delete(controller.deleteFolder);
 
+const viewController = require("./folderViewController");
+
+router.route("/folderView").get(viewController.getFromFolder);
+
 module.exports = router;
