7953e4fc432edc6e96933f5b15bffd8b1a253e5f
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 13:48:10 +0200

    Move FolderSection subcomponent into newly created NavigationFolderSection

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 import { faHouse, faRightToBracket, faRightFromBracket, faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 
 import NavigationItemLink from "./NavigationItemLink";
-import FolderList from "./FolderList";
-import CircleButton from "../buttons/CircleButton";
-
 import { useAuth } from "../../context/AuthProvider";
-import { useFolders } from "../../store/FolderProvider";
 
 import classes from "./Navigation.module.scss";
 
@@ -27,63 +22,6 @@ const Navigation = (props) => {
         icon: isLoggedIn ? faRightFromBracket : faRightToBracket
     };
 
-    const FolderSection = () => {
-        const [creatingFolder, setCreatingFolder] = useState(false);
-        const [newFolderName, setNewFolderName] = useState("");
-        const folders = useFolders();
-
-        useEffect(()=> {
-            if (show) {
-                console.log("load folders now pls");
-                folders.loadFolders();
-            }
-        }, []);
-
-        const edit = () => {
-            setCreatingFolder(!creatingFolder);
-        };
-
-        const createFolder = async (event) => {
-            event.preventDefault();
-            const data = { name: newFolderName };
-
-            const create = async () => {
-                return await fetch("http://localhost:4000/api/folder", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    credentials: "include",
-                    mode: "cors",
-                    body: JSON.stringify(data)
-                }).then(value => value.json())
-            };
-
-            create().then(folders.loadFolders);
-
-            setNewFolderName("");
-            setCreatingFolder(false);
-        };
-
-        return (
-            <>
-                <span className={classes.folders}>
-                    <label>Folders</label>
-                    <CircleButton tooltip="Add Folder" placement="right" onClick={edit}>
-                        <FontAwesomeIcon icon={faFolderPlus}/>
-                    </CircleButton>
-                </span>
-                <FolderList allFolders={folders.folders}/>
-                {creatingFolder &&
-                    <form onSubmit={createFolder}>
-                        <input type="text"
-                               autoFocus={true}
-                               value={newFolderName}
-                               onChange={event => setNewFolderName(event.target.value)}/>
-                    </form>
-                }
-            </>
-        );
-    };
-
     return (
         <header className={classes.wrapper}>
             <div className={classes.sidebar}>
@@ -91,7 +29,7 @@ const Navigation = (props) => {
                     <NavigationItemLink to="/" name="Home" icon={faHouse}/>
                     <NavigationItemLink to={loginData.link} name={loginData.text} icon={loginData.icon}/>
 
-                    {show && <FolderSection/>}
+                    {show && <NavigationFolderSection/>}
                 </ul>
             </div>
         </header>
---
+++ frontend/src/components/navigation/NavigationFolderSection.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
+import CircleButton from "../buttons/CircleButton";
+import FolderList from "./FolderList";
+
+import { useFolders } from "../../store/FolderProvider";
+
+import classes from "./Navigation.module.scss";
+
+const NavigationFolderSection = () => {
+    const [creatingFolder, setCreatingFolder] = useState(false);
+    const [newFolderName, setNewFolderName] = useState("");
+    const folders = useFolders();
+
+    useEffect(() => {
+        if (show) {
+            console.log("load folders now pls");
+            folders.loadFolders();
+        }
+    }, []);
+
+    const edit = () => {
+        setCreatingFolder(!creatingFolder);
+    };
+
+    const createFolder = async (event) => {
+        event.preventDefault();
+        const data = { name: newFolderName };
+
+        const create = async () => {
+            return await fetch("http://localhost:4000/api/folder", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+                mode: "cors",
+                body: JSON.stringify(data)
+            }).then(value => value.json())
+        };
+
+        create().then(folders.loadFolders);
+
+        setNewFolderName("");
+        setCreatingFolder(false);
+    };
+
+    return (
+        <>
+                <span className={classes.folders}>
+                    <label>Folders</label>
+                    <CircleButton tooltip="Add Folder" placement="right" onClick={edit}>
+                        <FontAwesomeIcon icon={faFolderPlus}/>
+                    </CircleButton>
+                </span>
+            <FolderList allFolders={folders.folders}/>
+            {creatingFolder &&
+                <form onSubmit={createFolder}>
+                    <input type="text"
+                           autoFocus={true}
+                           value={newFolderName}
+                           onChange={event => setNewFolderName(event.target.value)}/>
+                </form>
+            }
+        </>
+    );
+};
+
+export default NavigationFolderSection;
