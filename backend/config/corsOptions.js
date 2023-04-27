1851e6f8901941be3ba0e610acdb2aedf173fadc
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 17:49:22 +0200

    Create corsOptions configuration

---
+++ backend/config/corsOptions.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const whitelist = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(",") : []
+
+// Adapted from https://stackoverflow.com/a/55195449
+const corsOptions = {
+    origin: (origin, callback) => {
+        const isAllowed = !origin || whitelist.indexOf(origin) !== -1;
+        const showError = process.env.SHOW_CORS_DOMAIN_ERROR.toLowerCase() === "true";
+        callback(!isAllowed && showError ? new Error(`Domain ${origin} not allowed by CORS`) : null, isAllowed);
+    },
+    // origin: "http://localhost:8080",
+    credentials: true
+};
+
+module.exports = corsOptions;
