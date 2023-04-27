bcb3ab867ea9dd5e1302d3f09ada18fe34f0ea83
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 13:36:35 +0200

    Add optional show property to NavigationItemLink component

--- frontend/src/components/navigation/NavigationItemLink.js
+++ frontend/src/components/navigation/NavigationItemLink.js
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import NavigationItem from "./NavigationItem";
 
-const NavigationItemLink = ({ name, to, icon, children, ...props }) => {
-    return (
+const NavigationItemLink = ({ name, to, icon, children, show = true, ...props }) => {
+    return show ? (
         <NavigationItem name={name} {...props}>
             <Link to={to}>
                 <FontAwesomeIcon icon={icon}/>
@@ -13,7 +13,7 @@ const NavigationItemLink = ({ name, to, icon, children, ...props }) => {
             </Link>
             {children}
         </NavigationItem>
-    );
+    ) : "";
 };
 
 export default NavigationItemLink;
