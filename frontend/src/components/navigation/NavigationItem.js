ef86f340ffefe5bb8d3f60a71dcdc33980a21b0e
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 13:55:30 +0200

    Update NavigationItem to use MUI Tooltip instead of custom tooltip

--- frontend/src/components/navigation/NavigationItem.js
+++ frontend/src/components/navigation/NavigationItem.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Tooltip } from "@mui/material";
 
 const NavigationItem = ({ name, children, ...props }) => {
     return (
-        <li tooltip={name} tooltip-position="right" {...props}>
-            {children}
-        </li>
+        <Tooltip title={name} placement="right" enterDelay={1500} leaveDelay={200}>
+            <li {...props}>
+                {children}
+            </li>
+        </Tooltip>
     );
 };
 
