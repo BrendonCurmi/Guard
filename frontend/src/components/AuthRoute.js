6d0bb9163037e48fce28b426366b8c4cf8d05204
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 17:01:17 +0200

    Add customisable login status to AuthRoute

--- frontend/src/components/AuthRoute.js
+++ frontend/src/components/AuthRoute.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 
-const AuthRoute = ({}) => {
+const AuthRoute = ({ loginStatus = true }) => {
     const { auth } = useAuth();
     const location = useLocation();
+    const access = !auth?.accessToken === !loginStatus;//todo test this
     return (
-        auth?.accessToken
+        access
             ? <Outlet/>
             : <Navigate to="/login" state={{ from: location }} replace/>
     );
