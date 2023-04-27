34c9713f4a0b10893a4efc21eb4622cfaa7dd607
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 17:50:16 +0200

    Add cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,10 @@ require("dotenv").config();
 // Import middleware
 app.use(express.json());
 
+// Import CORS middleware
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+app.use(cors(corsOptions));
+
 const port = process.env.SERVER_PORT
 app.listen(port, () => console.log("Server is up"));
