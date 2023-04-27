cd01496a31668527878f91cb42d6bda8b1b32770
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 20:47:16 +0200

    Update PinController with Profile

--- backend/pins/pinController.js
+++ backend/pins/pinController.js
@@ -1,83 +1,10 @@
 const PinTemplate = require("./pins.model");
-const { moveToTrash } = require("../trash/trashController");
+const Profile = require("../profile");
 
-/**
- * Retrieves all accounts, excluding pw field.
- */
-exports.getAllPins = async (req, res) => {
-    const data = await PinTemplate.find().select("-pin");
-    requestCatch({ "pins": data }, res);
-};
-
-
-exports.createPins = (req, res) => {
-    new PinTemplate({
-        title: req.body.title,
-        pin: req.body.pin
-    })
-        .save()
-        .then(data => {
-            res.status(201).json(data);
-        })
-        .catch(err => {
-            res.status(400).json({ message: err.message });
-        });
-};
-
-
-/**
- * Deletes account with specified id.
- * @param req the request.
- * @param res the response.
- * @returns {Promise<void>} Returns HTTP 200 if successful;
- * Otherwise HTTP 410 if unsuccessful.
- */
-exports.deletePin = async (req, res) => {
-    try {
-        await moveToTrash(PinTemplate, "pins", req);
-        res.status(200).json({ message: "ok" });
-    } catch (err) {
-        res.status(500).json({ err: err.message });
+class PinController extends Profile {
+    constructor() {
+        super(PinTemplate, "pins", "pin");
     }
-};
+}
 
-exports.getPinById = async (req, res) => {
-    const data = await PinTemplate.findById(req.params.id).select("-pin");
-    requestCatch(data, res);
-};
-
-exports.updateById = async (req, res) => {
-    const data = PinTemplate.findByIdAndUpdate(req.params.id, req.body)
-        .then(data => {
-            res.sendStatus(204);
-        }).catch(err => {
-            console.log(err);
-            res.status(400).json({ message: err.message });
-        });
-};
-
-
-
-const requestCatch = (data, res) => {
-    try {
-        res.json(data);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
-
-
-
-
-
-/**
- * Retrieves only the credentials of the specified account,
- * while updating account's lastAccess property.
- * @param req the request.
- * @param res the response.
- * @returns {Promise<void>}
- */
-exports.getCredentialsById = async (req, res) => {
-    const data = await PinTemplate.findOneAndUpdate({ _id: req.params.id }, { lastAccess: Date.now() }).select("pin");
-    requestCatch(data, res);
-};
+module.exports = PinController;
