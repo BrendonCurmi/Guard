9acb07605d310dea27d3d951d36e7d0aba9ec966
BrendonCurmi <brendoncrm@gmail.com>
2023-04-27 19:10:11 +0200

    Create account routes

---
+++ backend/routes/accountRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const AccountTemplate = require("../models/accounts.model");
+
+router.post("/test", (req, res) => {
+    const test = new AccountTemplate({
+        site: req.body.site,
+        title: req.body.title,
+        pw: req.body.pw
+    });
+    test.save()
+        .then(data => {
+            res.status(201).json(data);
+        })
+        .catch(err => {
+            res.status(400).json({ message: err.message });
+        });
+});
+
+const accountController = require("../controllers/accountController");
+
+router.route("/accounts").get(accountController.getAllAccounts);
+
+// router.route("/credentials/:id").get(accountController.getCredentials);
+
+router.route("/account/:id")
+    .get(accountController.getAccountById)
+    .put(accountController.updateById)
+    .delete(accountController.deleteAccount);
+
+router.route("/account/:id/credentials")
+    .get(accountController.getCredentialsById);
+
+router.route("/search/:str")
+    .get(accountController.searchByString);
+
+module.exports = router;
