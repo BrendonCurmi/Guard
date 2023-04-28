f67e948a04051077364b6f2f3c241d0bcc3e0354
BrendonCurmi <brendoncrm@gmail.com>
2023-04-28 15:53:08 +0200

    Create users model

---
+++ backend/users/users.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    pw: {
+        type: String,
+        required: true
+    },
+    lastAccess: { type: Date, default: Date.now, required: true }
+});
+
+module.exports = mongoose.model("UserTemplate", schema, "users");
