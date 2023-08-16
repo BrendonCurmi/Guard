29c0bb86a59fc8ae25f79cb4980b140e0155e8f6
BrendonCurmi <brendoncrm@gmail.com>
2023-08-16 18:16:31 +0200

    Create file for API urls and endpoints

---
+++ frontend/src/utils/API.js
@@ -0,0 +1,4 @@
+export const API = "http://localhost:4000/api/auth";
+export const CREATE_API = API + "/user";
+export const LOGIN_API = API + "/login";
+export const REFRESH_API = API + "/refreshToken";
