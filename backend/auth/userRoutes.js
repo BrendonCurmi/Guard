6b90bbfa86c7d1690890d63ce9620d7a17d870df
BrendonCurmi <brendoncrm@gmail.com>
2023-08-16 21:34:52 +0200

    Implement validation in user routes

--- backend/auth/userRoutes.js
+++ backend/auth/userRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("./userController");
+const validateMiddleWare = require("../middleware/validate");
+const { validateUser } = require("./users.model");
 
-router.route("/user").post(UserController.createUser);
+router.route("/user").post([validateMiddleWare(validateUser)], UserController.createUser);
 
 router.route("/login").post(UserController.loginUser);
 
