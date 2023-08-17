a561e4b23c60fee75e788666bc8932b22baf20df
BrendonCurmi <brendoncrm@gmail.com>
2023-08-17 12:23:06 +0200

    Make AuthRoute only to authenticated routes

--- frontend/src/components/AuthRoute.js
+++ frontend/src/components/AuthRoute.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 
-const AuthRoute = ({ loginStatus = true }) => {
-    const { auth } = useAuth();
+const AuthRoute = () => {
+    const { authenticated } = useAuth();
     const location = useLocation();
-    const access = !auth?.accessToken === !loginStatus;//todo test this
     return (
-        access
+        authenticated
             ? <Outlet/>
             : <Navigate to="/login" state={{ from: location }} replace/>
     );
