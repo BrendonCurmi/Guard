373e4634892724b8312d37641ac1e510850ab6e5
BrendonCurmi <brendoncrm@gmail.com>
2023-08-17 20:13:00 +0200

    Update logout mechanism and clear encryption key and token cookies

--- frontend/src/pages/Logout/index.js
+++ frontend/src/pages/Logout/index.js
@@ -1,25 +1,35 @@
 import React, { useEffect } from "react";
-
 import { useNavigate } from "react-router-dom";
+
+import { safeFetch } from "../../utils/SafeFetch";
+import { setEncryptionKey } from "../../../security/EncryptionKeyUtils";
 import { useAuth } from "../../context/AuthProvider";
+
+import { LOGOUT_API } from "../../utils/API";
 
 // Milliseconds to wait before redirecting
 const redirectionWait = 3000;
 
 /**
- * Logout page that logs out user and redirects
- * them to /login page.
+ * Logout page that logs out user and redirects them to /login page.
  * @constructor
  */
 const Logout = () => {
     const navigate = useNavigate();
     const { setAuth, setPersist } = useAuth();
-    useEffect(() => {
+
+    const clear = () => {
         setAuth({});
         setPersist(false);
+        setEncryptionKey(null);
+        navigate("/login");
+    };
 
-        setTimeout(() => navigate("/login"), redirectionWait)
-    }, [])
+    useEffect(() => {
+        safeFetch(LOGOUT_API, "POST")
+            .then(() => setTimeout(clear, redirectionWait))
+            .catch(console.log);
+    }, []);
     return <p>Logging out...</p>;
 };
 
