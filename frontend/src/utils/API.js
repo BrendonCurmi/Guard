14748f5f9aa8092e2b262ab2a17ed83dd1bf1dc4
BrendonCurmi <brendoncrm@gmail.com>
2023-08-17 20:11:10 +0200

    Add logout API endpoint to API

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -1,4 +1,5 @@
 export const API = "http://localhost:4000/api/auth";
 export const CREATE_API = API + "/user";
 export const LOGIN_API = API + "/login";
+export const LOGOUT_API = API + "/logout";
 export const REFRESH_API = API + "/refreshToken";
