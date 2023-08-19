7e559e68f8a0d0782728a6855e2d97200d73d015
BrendonCurmi <brendoncrm@gmail.com>
2023-08-19 11:17:59 +0200

    Push

--- backend/security/encrypt.js
+++ backend/security/encrypt.js
@@ -5,7 +5,8 @@ const {
 } = require("node:crypto");
 
 const hashKey = (authKey, salt) => {
-    const authHash = pbkdf2Sync(authKey, salt, 2, 256, "sha256");
+    const iterations = 2;//todo 10,000
+    const authHash = pbkdf2Sync(authKey, salt, iterations, 256, "sha256");
     const key = scryptSync(authHash, salt, 256);
     return key.toString("base64");
 };
