dc769355754914c826844cfd1d92a6c7b3a60586
BrendonCurmi <brendoncrm@gmail.com>
2023-08-28 23:57:31 +0200

    Create FolderCache

---
+++ frontend/src/utils/FolderCache.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { getVault } from "./VaultCache";
+import { decryptData } from "../../security/SecurityUtils";
+
+// Cache for the folder ids and names
+let folderCache = {};
+
+export const loadFoldersFromCache = () => {
+    const allFolders = getVault()["folders"];
+    Object.keys(allFolders).map(key => {
+        const { _id, name } = allFolders[key];
+        folderCache[_id] = decryptData(name);
+    });
+    console.log(folderCache);
+}
+
+export const getFoldersCache = () => {
+    return { ...folderCache }; // Clone to prevent direct modification
+};
