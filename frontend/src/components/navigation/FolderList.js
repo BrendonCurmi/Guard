558be0e7844a0127ea6fdc2cbe99fece8274c26d
BrendonCurmi <brendoncrm@gmail.com>
2023-08-30 21:31:44 +0200

    Add custom event listener to update FolderList on folderCache update

--- frontend/src/components/navigation/FolderList.js
+++ frontend/src/components/navigation/FolderList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { faFolder } from "@fortawesome/free-solid-svg-icons";
 import NavigationFolderItem from "./NavigationFolderItem";
 import { encode } from "../../utils/URLUtils";
+import { getFoldersCache } from "../../utils/FolderCache";
 
 import classes from "./FolderList.module.scss";
 
-const FolderList = ({ allFolders }) => {
+const FolderList = () => {
+    const [folderCache, setFolderCache] = useState(getFoldersCache());
+
+    useEffect(() => {
+        // Listen for the folder update event to update state and reload component
+        const updateCacheListener = event => setFolderCache(event.detail);
+
+        document.addEventListener("folderCacheUpdated", updateCacheListener);
+
+        return () => document.removeEventListener("folderCacheUpdated", updateCacheListener);
+    }, []);
+
     const folderListItems = (name, i) => {
         const url = encode(name);
         return <NavigationFolderItem to={`/folder/${url}`}
@@ -16,8 +28,8 @@ const FolderList = ({ allFolders }) => {
     return (
         <ul className={classes.navFolderList}>
             {
-                allFolders && Object.keys(allFolders).map(id => {
-                    const name = allFolders[id];
+                folderCache && Object.keys(folderCache).map(id => {
+                    const name = folderCache[id];
                     return folderListItems(name, id);
                 })
             }
