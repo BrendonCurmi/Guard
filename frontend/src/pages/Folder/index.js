63acff9f28a2f4afc58349c05630f67db5c9754a
BrendonCurmi <brendoncrm@gmail.com>
2023-08-30 16:40:58 +0200

    Replace useFolders in Folder page to vault solution

--- frontend/src/pages/Folder/index.js
+++ frontend/src/pages/Folder/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate, useParams } from "react-router-dom";
 
 import FullView from "../../components/views/FullView";
 import CircleButton from "../../components/buttons/CircleButton";
+
 import { decode, encode } from "../../utils/URLUtils";
-import { useFolders } from "../../store/FolderProvider";
+import { getFoldersCache } from "../../utils/FolderCache";
+import { getVault } from "../../utils/VaultCache";
 
 const API = "http://localhost:4000/api/folderView";
 
@@ -15,7 +17,48 @@ const Folder = () => {
     const folderName = decode(name);
 
     const navigate = useNavigate();
-    const folders = useFolders();
+    const folders = getFoldersCache();
+
+    function getIdFromFolderName(folders, folderName) {
+        for (const id in folders) {
+            if (folders[id] === folderName) {
+                return id;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Gets an array of all the items from the specified
+     * vault data that belong to the specified folder.
+     * @param vaultData the encrypted vault data.
+     * @param folderId the id of the folder.
+     * @returns {Array} array of all items in folder.
+     */
+    function getItemsWithFolderId(vaultData, folderId) {
+        const folderItems = [];
+        for (const type in vaultData) {
+            const items = vaultData[type];
+            for (const item of items) {
+                if (item.folders && item.folders.includes(folderId)) {
+                    item.type = type;
+                    folderItems.push(item);
+                }
+            }
+        }
+        return folderItems;
+    }
+
+    // Get items belonging to the current folder and organise them into a data object
+    const folderData = {};
+    const folderId = getIdFromFolderName(folders, folderName);
+    const itemsInFolder = getItemsWithFolderId(getVault(), folderId);
+    Object.keys(itemsInFolder).map(key => {
+        const item = itemsInFolder[key];
+        const itemType = item.type;
+        if (folderData[itemType] === undefined) folderData[itemType] = [];
+        folderData[itemType].push(item);
+    });
 
     const deleteFolderHandler = async () => {
         const rawResponse = await fetch(`http://localhost:4000/api/folder/`, {
@@ -27,7 +70,7 @@ const Folder = () => {
         if (rawResponse.status === 200) {
             const content = await rawResponse.json();
             // console.log(content);
-            folders.loadFolders();
+            // folders.loadFolders();
             navigate("/pass");
         }
     };
@@ -61,6 +104,7 @@ const Folder = () => {
             title: "Are you sure?",
             msg: "Do you really want to delete this? It can't be recovered once it's deleted"
         }}
+        loadItems={folderData}
         loadApi={`${API}?f=${encode(folderName)}`}
         loadDeps={[name]}/>;
 };
