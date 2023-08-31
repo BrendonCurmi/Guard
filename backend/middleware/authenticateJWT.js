38edcb5d7accade1cd1367f0d61cacf5d2103c85
BrendonCurmi <brendoncrm@gmail.com>
2023-08-31 16:27:17 +0200

    Create authenticateJWT middleware to authenticate JSON Web Tokens in backend

---
+++ backend/middleware/authenticateJWT.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const authenticateJWT = (req, res, next) => {
+    const token = req.cookies["x-auth-token"];
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+            req.userId = decoded.userId;
+            next();
+        } catch (ex) {
+            res.sendStatus(403);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+module.exports = authenticateJWT;
