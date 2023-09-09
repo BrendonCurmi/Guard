8baf179ad656270e2890e2b73656616e7307ff94
BrendonCurmi <brendoncrm@gmail.com>
2023-09-09 23:59:21 +0200

    Remove refresh API

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -4,7 +4,6 @@ export const API = "http://localhost:4000/api";
 export const CREATE_API = API + "/auth/user";
 export const LOGIN_API = API + "/auth/login";
 export const LOGOUT_API = API + "/auth/logout";
-export const REFRESH_API = API + "/auth/refreshToken";
 
 // Vault
 export const VAULT_API = API + "/vault";
