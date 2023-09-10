e28f2c5981ba25b8e620ab49fee80f1ec80123bd
BrendonCurmi <brendoncrm@gmail.com>
2023-09-11 00:25:23 +0200

    Update middleware validation

--- backend/middleware/validate.js
+++ backend/middleware/validate.js
@@ -1,8 +1,10 @@
 // Adapted from https://gist.github.com/stongo/6359042
-module.exports = (validator) => {
+const validateMiddleware = (validator) => {
     return (req, res, next) => {
         const { error } = validator(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
+        if (error) return res.status(400).send({ err: error.details[0].message });
         next();
     }
-}
+};
+
+module.exports = validateMiddleware;
