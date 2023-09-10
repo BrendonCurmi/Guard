590c8828658da93f6482c9d17713c5be249364a8
BrendonCurmi <brendoncrm@gmail.com>
2023-09-10 20:37:26 +0200

    Add JOI validation to Folders model

--- backend/components/folders/folders.model.js
+++ backend/components/folders/folders.model.js
@@ -1,17 +1,29 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const foldersSchema = new mongoose.Schema({
+const pattern = /[a-zA-Z0-9 _]/;
+
+const schema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         default: "Folder",
         unique: true,
-        validate: /[a-zA-Z0-9 _]/
+        validate: pattern
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "users"
-    },
+    }
 });
 
-module.exports = mongoose.model("FolderTemplate", foldersSchema, "folders");
+const FolderTemplate = mongoose.model("FolderTemplate", schema, "folders");
+
+const validateFolder = (folder) => {
+    const schema = Joi.object({
+        email: Joi.string().trim().pattern(pattern).required()
+    });
+    return schema.validate(folder);
+};
+
+module.exports = { FolderTemplate, validateFolder };
