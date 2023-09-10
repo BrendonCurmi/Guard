6ff3688d4b067aa7804a6436bf7f17a34c6d6b5c
BrendonCurmi <brendoncrm@gmail.com>
2023-09-10 22:45:33 +0200

    Remove unused searchByString function from Account controller

--- backend/components/accounts/accountController.js
+++ backend/components/accounts/accountController.js
@@ -5,22 +5,6 @@ class AccountController extends Profile {
     constructor() {
         super(AccountTemplate, "accounts", "pw");
     }
-
-    searchByString = async (req, res) => {
-        let str = { "$regex": req.params.str };
-        await AccountTemplate.find({
-            $or: [
-                { "site": str },
-                { "title": str },
-                { "email": str },
-                { "username": str }
-            ]
-        }).then(data => {
-            res.status(201).json(data);
-        }).catch(err => {
-            res.status(500).json({ message: err.message });
-        });
-    };
 }
 
 module.exports = AccountController;
