0f5503de6d6b5f895d3941ebba57809898d584c9
BrendonCurmi <brendoncrm@gmail.com>
2023-09-10 22:36:51 +0200

    Add validation to Note routes

--- backend/components/notes/noteRoutes.js
+++ backend/components/notes/noteRoutes.js
@@ -4,13 +4,16 @@ const router = express.Router();
 const NoteController = require("./noteController");
 const noteController = new NoteController();
 
-router.route("/notes").get(noteController.getAll);
+const validateMiddleWare = require("../../middleware/validate");
+const { validateNote } = require("./notes.model");
+
+router.route("/notes").get([validateMiddleWare(validateNote)], noteController.getAll);
 
 router.route("/note").post(noteController.create)
 
 router.route("/note/:id")
     .get(noteController.getById)
-    .put(noteController.updateById)
+    .put([validateMiddleWare(validateNote)], noteController.updateById)
     .delete(noteController.delete);
 
 module.exports = router;
