008e950cb98f38c2c212b1a48649dd1ae73b71e6
BrendonCurmi <brendoncrm@gmail.com>
2023-09-10 20:48:02 +0200

    Add validation to Account routes

--- backend/components/accounts/accountRoutes.js
+++ backend/components/accounts/accountRoutes.js
@@ -4,13 +4,16 @@ const router = express.Router();
 const AccountController = require("./accountController");
 const accountController = new AccountController();
 
+const validateMiddleWare = require("../../middleware/validate");
+const { validateAccount, validateUpdateAccount } = require("./accounts.model");
+
 router.route("/accounts").get(accountController.getAll);
 
-router.route("/account").post(accountController.create);
+router.route("/account").post([validateMiddleWare(validateAccount)], accountController.create);
 
 router.route("/account/:id")
     .get(accountController.getById)
-    .put(accountController.updateById)
+    .put([validateMiddleWare(validateUpdateAccount)], accountController.updateById)
     .delete(accountController.delete);
 
 module.exports = router;
