2ca95bddd9aeb94f48672838f56cfb28cffdb3ab
BrendonCurmi <brendoncrm@gmail.com>
2023-09-11 00:35:20 +0200

    Update getFolderById in folderController

--- backend/components/folders/folderController.js
+++ backend/components/folders/folderController.js
@@ -27,12 +27,9 @@ exports.deleteFolder = async (req, res) => {
 };
 
 exports.getFolderById = async (req, res) => {
-    try {
-        const data = await FolderTemplate.findById(req.params.id);
-        res.json(data);
-    } catch (err) {
-        res.status(400).json({ err: err.message });
-    }
+    FolderTemplate.findById(req.params.id)
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json({ err: err.message }));
 };
 
 exports.updateById = async (req, res) => {
