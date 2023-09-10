c2f633ceaff82529b9a06bba64bc24187d11b709
BrendonCurmi <brendoncrm@gmail.com>
2023-09-10 23:56:39 +0200

    Update NoteController

--- backend/components/notes/noteController.js
+++ backend/components/notes/noteController.js
@@ -1,9 +1,9 @@
-const NoteTemplate = require("./notes.model");
+const { NoteTemplate } = require("./notes.model");
 const Profile = require("../profile");
 
 class NoteController extends Profile {
     constructor() {
-        super(NoteTemplate, "notes", "note");
+        super(NoteTemplate, "notes");
     }
 }
 
